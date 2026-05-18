2420ea6d1e4a0430e527677993a0287793126847
Farhad Nuri <231006312@eastdelta.edu.bd>
2026-05-18 13:55:09 +0600

    Added Boilarplate Template For Login & Register

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const { heroui } = require('@heroui/theme')
+
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'  // ← add this
+  ],
+  plugins: [heroui()]
+}
